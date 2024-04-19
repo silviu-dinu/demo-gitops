3560ae7648c787501cb832de923bd64d6e841e9e
Silviu Dinu <silviudinu@mbp-16.local>
2024-04-19 14:44:09 +0400

    Use GH_PAT for merging pull requests from workflows.

--- .github/workflows/workflow-steps.sh
+++ .github/workflows/workflow-steps.sh
@@ -70,7 +70,10 @@ create_overlay_pull_request() { # See https://stackoverflow.com/a/75308228
 
 auto_merge_overlay_pull_request() {
   echo "Auto-merging overlay pull request..."
-  gh pr merge $BRANCH --squash --auto
+  GH_TOKEN=$GH_PAT gh pr merge $BRANCH --squash --auto
+
+  # echo "Auto-merging DISABLED to allow triggering overlay-deploy."
+  # gh pr merge $BRANCH --squash --auto
 }
 
 apply_overlays() {
